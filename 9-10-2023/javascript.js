fabd7db871a7a27e83025c7da5f62bcb79143988
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2023-10-09 09:54:34 +0700

    upload assignment 9-10-2023

---
+++ 9-10-2023/javascript.js
@@ -0,0 +1,14 @@
+// document.write('hello')
+function vidu(){
+	alert('Xin chao')
+}
+
+var fruit = 'apples';
+var numfruit = 12;
+numfruit = numfruit + 20;
+var temp ="There are " + numfruit + " "+ fruit + ".";
+document.write(temp);
+
+function displayName(name){
+	alert('Xin chao: ' + name);
+}
